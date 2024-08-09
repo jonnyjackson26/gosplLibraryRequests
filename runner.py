bce7bf2bdde24ce038a3e11284619f895778e346
jonnyjackson26 <jrsjackson26@gmail.com>
2024-08-09 16:49:56 -0600

    with title paages

--- runner.py
+++ runner.py
@@ -4,30 +4,35 @@ import os
 import time
 
 books = [
-    {"numOfChapters": 22, "title": "1-nephi", "uri": "1-ne"},
-    {"numOfChapters": 33, "title": "2-nephi", "uri": "2-ne"},
-    {"numOfChapters": 7, "title": "jacob", "uri": "jacob"},
-    {"numOfChapters": 1, "title": "enos", "uri": "enos"},
-    {"numOfChapters": 1, "title": "jarom", "uri": "jarom"},
-    {"numOfChapters": 1, "title": "omni", "uri": "omni"},
-    {"numOfChapters": 1, "title": "words-of-mormon", "uri": "w-of-m"},
-    {"numOfChapters": 29, "title": "mosiah", "uri": "mosiah"},
-    {"numOfChapters": 63, "title": "alma", "uri": "alma"},
-    {"numOfChapters": 16, "title": "helaman", "uri": "hel"},
-    {"numOfChapters": 30, "title": "3-nephi", "uri": "3-ne"},
-    {"numOfChapters": 1, "title": "4-nephi", "uri": "4-ne"},
-    {"numOfChapters": 9, "title": "mormon", "uri": "morm"},
-    {"numOfChapters": 15, "title": "ether", "uri": "ether"},
-    {"numOfChapters": 10, "title": "moroni", "uri": "moro"}
+    {"numOfChapters": 1, "title": "title", "uri": "bofm-title", "isTitlePage":True},
+    {"numOfChapters": 1, "title": "introduction", "uri": "introduction", "isTitlePage":True},
+    {"numOfChapters": 1, "title": "three", "uri": "three", "isTitlePage":True},
+    {"numOfChapters": 1, "title": "eight", "uri": "eight", "isTitlePage":True},
+    {"numOfChapters": 1, "title": "js", "uri": "js", "isTitlePage":True},
+    {"numOfChapters": 22, "title": "1-nephi", "uri": "1-ne", "isTitlePage":False},
+    {"numOfChapters": 33, "title": "2-nephi", "uri": "2-ne", "isTitlePage":False},
+    {"numOfChapters": 7, "title": "jacob", "uri": "jacob", "isTitlePage":False},
+    {"numOfChapters": 1, "title": "enos", "uri": "enos", "isTitlePage":False},
+    {"numOfChapters": 1, "title": "jarom", "uri": "jarom", "isTitlePage":False},
+    {"numOfChapters": 1, "title": "omni", "uri": "omni", "isTitlePage":False},
+    {"numOfChapters": 1, "title": "words-of-mormon", "uri": "w-of-m", "isTitlePage":False},
+    {"numOfChapters": 29, "title": "mosiah", "uri": "mosiah", "isTitlePage":False},
+    {"numOfChapters": 63, "title": "alma", "uri": "alma", "isTitlePage":False},
+    {"numOfChapters": 16, "title": "helaman", "uri": "hel", "isTitlePage":False},
+    {"numOfChapters": 30, "title": "3-nephi", "uri": "3-ne", "isTitlePage":False},
+    {"numOfChapters": 1, "title": "4-nephi", "uri": "4-ne", "isTitlePage":False},
+    {"numOfChapters": 9, "title": "mormon", "uri": "morm", "isTitlePage":False},
+    {"numOfChapters": 15, "title": "ether", "uri": "ether", "isTitlePage":False},
+    {"numOfChapters": 10, "title": "moroni", "uri": "moro", "isTitlePage":False}
 ]
 
 languages=[
+    {"code":"deu","full-eng-title":"german"},
     {"code":"eng","full-eng-title":"english"},
     {"code":"spa","full-eng-title":"spanish"},
     {"code":"fra","full-eng-title":"french"},
     {"code":"por","full-eng-title":"portugese"},
     {"code":"ita","full-eng-title":"italian"},
-    {"code":"deu","full-eng-title":"german"},
 ]
 
 
@@ -37,7 +42,10 @@ def writeLinesToFile(lines, path):
             file.writelines(line for line in lines)
 
 def getData(language,book,chapter):
-    base_url = "https://www.churchofjesuschrist.org/study/api/v3/language-pages/type/content?lang={}&uri=/scriptures/bofm/{}/{}"  #https://www.churchofjesuschrist.org/study/api/v3/language-pages/type/content?lang=spa&uri=/scriptures/bofm/1-ne/5
+    if(book["isTitlePage"]):
+        base_url = "https://www.churchofjesuschrist.org/study/api/v3/language-pages/type/content?lang={}&uri=/scriptures/bofm/{}"
+    else:
+        base_url = "https://www.churchofjesuschrist.org/study/api/v3/language-pages/type/content?lang={}&uri=/scriptures/bofm/{}/{}"  #https://www.churchofjesuschrist.org/study/api/v3/language-pages/type/content?lang=spa&uri=/scriptures/bofm/1-ne/5
     url = base_url.format(language["code"], book["uri"], chapter) 
     print(url)
 
@@ -55,7 +63,7 @@ def getData(language,book,chapter):
         # Split the text into a list of verses based on the verse number pattern, and remove the numbers
         verses = re.split('\d+ ', cleaned_string)
         
-        pattern = r"Chapter \d+"
+        pattern = r"(Chapter|Capítulo|Kapitel|Chapitre) \d+"
         verses[0] = re.sub(pattern, "", verses[0])
         #verses[0] has a chapter desciption but it also contains book description and other stuff. this removes "Chapter X" thats also includded
 
